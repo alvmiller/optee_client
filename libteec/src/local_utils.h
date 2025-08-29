966e739156424ef625f5630e2e7c9031f7f75dc2
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-08-29 23:35:07 +0300

    Create local_utils.h

---
+++ libteec/src/local_utils.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#ifndef LIBTEEC_LOCAL_UTILS_H
+#define LIBTEEC_LOCAL_UTILS_H
+
+#define MIN(x, y) \
+	({ \
+		typeof(x) _x = (x); \
+		typeof(y) _y = (y); \
+		(void)(&_x == &_y); \
+		_x < _y ? _x : _y; \
+	})
+
+#endif /*LIBTEEC_LOCAL_UTILS_H*/
